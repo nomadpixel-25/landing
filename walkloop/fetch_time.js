8cea26d88bc3eb614a9a6ad9ef5ecabada6dad72
nomadpixel-25 <nomadpixel.25@gmail.com>
2025-02-01 19:51:53 +0000

    last html save time

--- walkloop/fetch_time.js
+++ walkloop/fetch_time.js
@@ -1,9 +1,23 @@
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const lastModified = new Date(document.lastModified).toLocaleString('en-GB', {
+//         timeZone: 'Europe/London',
+//         day: '2-digit',
+//         month: '2-digit',
+//         year: 'numeric',
+//         hour: '2-digit',
+//         minute: '2-digit',
+//         second: '2-digit'
+//     });
+//     document.getElementById('lastSavedTime').innerText = lastModified;
+// });
+
+
+
+
 async function fetchLatestCommitTime() {
     try {
-        const response = await fetch('https://api.github.com/repos/nomadpixel-25/landing/commits');
-        const data = await response.json();
-        const latestCommit = data[0];
-        const commitTime = new Date(latestCommit.commit.author.date).toLocaleString('en-GB', {
+        const lastModified = new Date(document.lastModified).toLocaleString('en-GB', {
             timeZone: 'Europe/London',
             day: '2-digit',
             month: '2-digit',
@@ -11,6 +25,17 @@ async function fetchLatestCommitTime() {
             hour: '2-digit',
             minute: '2-digit',
             second: '2-digit'
+        // const response = await fetch('https://api.github.com/repos/nomadpixel-25/landing/commits');
+        // const data = await response.json();
+        // const latestCommit = data[0];
+        // const commitTime = new Date(latestCommit.commit.author.date).toLocaleString('en-GB', {
+        //     timeZone: 'Europe/London',
+        //     day: '2-digit',
+        //     month: '2-digit',
+        //     year: 'numeric',
+        //     hour: '2-digit',
+        //     minute: '2-digit',
+        //     second: '2-digit'
         });
         document.getElementById('commitTime').innerText = commitTime;
     } catch (error) {
